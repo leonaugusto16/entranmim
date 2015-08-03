239a95c7a0f7717ab4002ab4e6e0f1e79432b626
Claudio Netto <nettinhorama@gmail.com>
2015-08-03 09:59:08 -0300

    Nova home forum pt2 [forum_CGSN]

--- forum/best_users.php
+++ forum/best_users.php
@@ -1,7 +1,7 @@
 <div class="panel-group" id="panel-best_users">
   <div class="panel panel-default">
     <div class="panel-heading text-right" data-toggle="collapse" data-parent="#panel-best_user" href="#panel-element-best_users" title="Clique para encolher/expandir">
-       <span class="panel-title">Top usuários</a>
+       <span class="panel-title">Top usuários</span>
     </div>
     <div id="panel-element-best_users" class="panel-collapse collapse in">
       <div class="panel-body">
